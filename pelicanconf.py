22a750042258c62b2d0c5e8f98e3194f9dcd99fa
Jan Papousek <jan.papousek@gmail.com>
2017-03-26 12:06:54 +0200

    more customization

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,15 +5,15 @@ from __future__ import unicode_literals
 AUTHOR = 'Jan Papousek'
 SITENAME = '...'
 SITEURL = ''
-SITELOGO = 'content/images/papousek.jpeg'
+SITELOGO = 'images/papousek.jpeg'
 
-THEME = 'Flex'
+THEME = 'notmyidea'
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
-DEFAULT_LANG = 'cs'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -23,16 +23,22 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('adaptive learning group', 'http://www.fi.muni.cz/adaptivelearning/'),
+    ('google scholar', 'https://scholar.google.cz/citations?user=ozn2O8gAAAAJ')
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/papousek'),
+          ('twitter', 'https://twitter.com/jan_papousek'),
+          ('linkedin', 'https://www.linkedin.com/in/janpapousek/'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATH = './plugins'
+PLUGINS = ['ipynb.markup']
